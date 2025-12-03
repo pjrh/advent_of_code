28d5dd71a348fcb83490aa01244b0dc434425275
pjrh <bald-egg@mail.rhio.uk>
2025-12-03 12:11:25 +0000

    add recursive solution

--- 2025/day03.R
+++ 2025/day03.R
@@ -5,10 +5,7 @@ library(tictoc)
 
 tic("Input prep")
 
-input = readr::read_file("day03_input.txt") |>
-  str_split("\n") |>
-  unlist() %>%
-  map(~as.numeric(str_split_1(.x,"")))
+input <- readr::read_file("day03_test.txt")
 
 toc()
 
@@ -21,9 +18,11 @@ max_joltage <- function(x) {
   x[[ind1]]*10+x[[ind2]]
 }
 
-input %>%
-  map_vec(~max_joltage(.x)) %>%
-  sum() %>%
+input |>
+  str_split_1("\n") |>
+  map(~as.numeric(str_split_1(.x,""))) |>
+  map_vec(~max_joltage(.x)) |>
+  sum() |>
   (\(x) print(paste0("Part 1: ", x)))()
 
 toc()
@@ -47,9 +46,37 @@ max_joltage_p2 <- function(x) {
   x[[ind1]]*10^11+x[[ind2]]*10^10+x[[ind3]]*10^9+x[[ind4]]*10^8+x[[ind5]]*10^7+x[[ind6]]*10^6+x[[ind7]]*10^5+x[[ind8]]*10^4+x[[ind9]]*10^3+x[[ind10]]*10^2+x[[ind11]]*10^1+x[[ind12]]*10^0
 }
 
-input %>%
-  map_vec(~max_joltage_p2(.x)) %>%
-  sum() %>%
+input |>
+  str_split_1("\n") |>
+  map(~as.numeric(str_split_1(.x,""))) |>
+  map_vec(~max_joltage_p2(.x)) |>
+  sum() |>
   (\(x) print(paste0("Part 2: ", x)))()
+
+toc()
+
+tic("Part 2 recursive")
+
+# x is a vector of numbers
+# n is the number of digits (batteries) required in the answer
+max_joltage_p2_r <- function(x,n) {
+  if (n==0) return(0) # Stop if no digits remaining to find
+  
+  # Ignore the trailing digits so there are space for the remaining 10s places
+  x_head <- x[1:(length(x)-(n-1))]
+  # Find the index of the largest digit in the relevant portion of the batteries
+  ind <- which(x_head == max(x_head))[1]
+  
+  # Returns the value of the nth digit in correct 10s place
+  # plus the value of the n-1th digit in the correct 10s place
+  return(x[[ind]]*10^(n-1) + max_joltage_p2_r(x[(ind+1):length(x)], n-1))
+}
+
+input |>
+  str_split_1("\n") |> # Split input into rows
+  map(~as.numeric(str_split_1(.x,""))) |> # Make each row a vector of numbers
+  map_vec(~max_joltage_p2_r(.x, 12)) |> # Run the function on each row
+  sum() |> # Sum the result
+  (\(x) print(paste0("Part 2 (recursive): ", x)))() # Enjoy the result
 
 toc()
